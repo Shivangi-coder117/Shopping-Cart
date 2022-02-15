28c999f6e87888f9675161d2689fd81583f4fa3a
Shivangi Singh <ss7433@srmist.edu.in>
2022-02-15 19:43:48 +0530

    Add Total Price

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ class App extends React.Component {
     this.state = {
         products :[
         {
-        price : 99,
+        price : 6000,
         title : 'Watch',
         qty : 1,
-        img : '',
+        img : 'https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main1.png?v=1640628318',
         id:1
 
         },
         {
-        price : 999,
+        price : 65000,
         title : 'Phone',
         qty : 10,
-        img : '',
+        img : 'https://oasis.opstatics.com/content/dam/oasis/page/2021/operation/1217/homepage/mo/02-OnePlus-9.jpg',
         id:2
         },
         {
             price : 999,
         title : 'Laptop',
         qty : 4,
-        img : '',
+        img : 'https://cdn.thewirecutter.com/wp-content/media/2020/12/macbook-2048px-9-2x1-1.jpg?auto=webp&quality=75&crop=2:1&width=1024',
         id:3
 
         }
@@ -70,6 +70,19 @@ products.forEach((product) => {
   return count;
 }
 
+getCartTotal = () =>{
+   const {products} = this.state;
+    
+   let cartTotal = 0;
+
+   products.map((product)=> {
+    cartTotal = cartTotal + product.qty * product.price
+     
+   })
+   return cartTotal;
+}
+
+
 handleDeleteProduct = (id)=>{
 const{products}=this.state;
 const items = products.filter((item) => item.id !== id);
@@ -89,6 +102,7 @@ this.setState({
          onDecreaseQuantity={this.handleDecreaseQuantity}
         onDeleteProduct = {this.handleDeleteProduct}
         />
+        <div style = {{padding : 10, fontSize : 20  }}>Total : {this.getCartTotal()}</div>
       </div>
     );
   }
